ab18dfdb2a1bb63d4c328b43802a422f9c63a854
Devin Chen <d3vin.chen@gmail.com>
2020-09-18 23:31:30 +0800

    remove key from source code

--- src/Application/config.js
+++ src/Application/config.js
@@ -1,2 +1,2 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyBPypOyA88JUwstHMc4YIgWZsyROdm_gF0";
-export const AMAP_API_KEY = "405065d496223d58ae3cdf811e12b546";
+export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+export const AMAP_API_KEY = process.env.REACT_APP_AMAP_API_KEY;
