790c0aac742aac1c5c91f531832876e3b31e4b61
Devin Chen <d3vin.chen@gmail.com>
2020-09-19 01:21:00 +0800

    test

--- src/Application/AMap/index.jsx
+++ src/Application/AMap/index.jsx
@@ -19,6 +19,7 @@ function AMap(props) {
       <p>process.env.REACT_APP_DEVIN:{process.env.REACT_APP_DEVIN}</p>
       <p>process.env.DEVIN2:{process.env.DEVIN2}</p>
       <p>process.env.NODE_ENV:{process.env.NODE_ENV}</p>
+      <p>process.env:{JSON.stringify(process.env)}</p>
 
       <Map
         amapkey={process.env.REACT_APP_AMAP_API_KEY}
