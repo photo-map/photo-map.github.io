82c291d9b14da953edc017141192a18380235837
Devin Chen <d3vin.chen@gmail.com>
2020-10-13 21:37:26 +0800

    add type define

--- src/Application/Map/GoogleMap/GoogleMapWrapper.jsx
+++ src/Application/Map/GoogleMap/GoogleMapWrapper.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { GoogleMap } from "react-google-maps";
 
+/**
+ * google.maps.LatLng
+ * @typedef {Object} LatLng
+ */
+
+/**
+ * This object is returned from various mouse events on the map and overlays, and contains all the fields shown below.
+ * Checkout the {@link https://developers.google.com/maps/documentation/javascript/reference/map#MouseEvent API document}
+ * @typedef {Object} MouseEvent
+ * @property {LatLng} latLng
+ */
+
+/**
+ * Checkout the {@link https://developers.google.com/maps/documentation/javascript/reference/map#Map.click API document} for this "click" event
+ * @param {MouseEvent} event
+ */
+function handleClick(event) {
+  // @type {LatLng} https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLng
+  const { latLng } = event;
+  console.log(`You click on the Google Maps, latLng: ${latLng.toString()}`);
+}
+
 const GoogleMapWrapper = (props) => (
   <GoogleMap
     defaultZoom={props.defaultZoom}
     defaultCenter={props.defaultCenter}
-    onClick={
-      /**
-       * https://developers.google.com/maps/documentation/javascript/reference/map#Map.click
-       * @param {MouseEvent} event https://developers.google.com/maps/documentation/javascript/reference/map#MouseEvent
-       */
-      (event) => {
-        // @type {google.maps.LatLng} https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLng
-        const { latLng } = event;
-        console.log(
-          `You click on the Google Maps, latLng: ${latLng.toString()}`
-        );
-      }
-    }
+    onClick={handleClick}
   >
     {props.children}
   </GoogleMap>
