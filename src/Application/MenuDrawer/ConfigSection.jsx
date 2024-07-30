d3bcf799611f0e393e9b009924d7be4ec5eff68b
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-07-30 22:18:34 +0800

    functional component

--- src/Application/MenuDrawer/ConfigSection.jsx
+++ src/Application/MenuDrawer/ConfigSection.jsx
@@ -1,39 +1,31 @@
-import React, { Component } from "react";
-import { Button } from "antd";
+import React, { useRef, useState } from 'react';
+import { Button } from 'antd';
 
-import { exportConfig, importConfig } from "./helpers";
+import { exportConfig, importConfig } from './helpers';
 
-export default class ConfigSection extends Component {
-  constructor(props) {
-    super(props);
-    this.downloadRef = React.createRef();
-    this.inputRef = React.createRef();
-    this.state = {
-      href: "{}",
-    };
-  }
+export default function ConfigSection() {
+  const downloadRef = useRef();
+  const inputRef = useRef();
+  const [href, setHref] = useState('{}');
 
-  handleDownload = () => {
-    this.setState({ href: exportConfig(localStorage) }, () => {
-      if (this.downloadRef && this.downloadRef.current) {
-        this.downloadRef.current.click();
-      } else {
-        alert("downloadRef or node is undefined!");
-      }
-    });
-  };
-
-  handleImport = () => {
-    if (this.inputRef && this.inputRef.current) {
-      this.inputRef.current.click();
+  const handleDownload = () => {
+    setHref(exportConfig(localStorage));
+    if (downloadRef.current) {
+      downloadRef.current.click();
     } else {
-      alert("inputRef or node is undefined!");
+      alert('downloadRef or node is undefined!');
     }
   };
 
-  // Solution A: https://stackoverflow.com/a/27116209/4685522
-  // Solution B: https://stackoverflow.com/a/48176648/4685522
-  handleChange = async (event) => {
+  const handleImport = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    } else {
+      alert('inputRef or node is undefined!');
+    }
+  };
+
+  const handleChange = async (event) => {
     const file = event.target.files[0];
     if (file) {
       const data = await new Response(file).text();
@@ -42,28 +34,26 @@ export default class ConfigSection extends Component {
     }
   };
 
-  render() {
-    return (
-      <div>
-        <h3>Import/Export application config</h3>
-        <a
-          ref={this.downloadRef}
-          href={this.state.href}
-          style={{ display: "none" }}
-          download="photo-map-config.json"
-        >
-          Download Config File
-        </a>
-        <Button onClick={this.handleDownload}>Export</Button>
-        <input
-          ref={this.inputRef}
-          className="import-config-file-helper"
-          type="file"
-          style={{ display: "none" }}
-          onChange={this.handleChange}
-        />
-        <Button onClick={this.handleImport}>Import</Button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h3>Import/Export application config</h3>
+      <a
+        ref={downloadRef}
+        href={href}
+        style={{ display: 'none' }}
+        download='photo-map-config.json'
+      >
+        Download Config File
+      </a>
+      <Button onClick={handleDownload}>Export</Button>
+      <input
+        ref={inputRef}
+        className='import-config-file-helper'
+        type='file'
+        style={{ display: 'none' }}
+        onChange={handleChange}
+      />
+      <Button onClick={handleImport}>Import</Button>
+    </div>
+  );
 }
